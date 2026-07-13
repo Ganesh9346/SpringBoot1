b15cf7a68e66bd6dc371f4dd727f26911f008a88
Ganesh <bandaruganesh876@gmail.com>
2026-07-13 14:32:27 +0530

    Dependency Injection

--- src/main/java/com/Ganesh/demo/DemoApplication.java
+++ src/main/java/com/Ganesh/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.Ganesh.demo;
 
+import com.Ganesh.demo.dependencyInjection.OrderService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,11 +11,13 @@ public class DemoApplication {
 	public static void main(String[] args) {
 
 		ApplicationContext container= SpringApplication.run(DemoApplication.class, args);
-		Student s1=container.getBean(Student.class);
-		s1.setName("Ramu");
-		s1.setAge(22);
-		System.out.println(s1.getName());
-		System.out.println(s1.getAge());
+//		Student s1=container.getBean(Student.class);
+//		s1.setName("Ramu");
+//		s1.setAge(22);
+//		System.out.println(s1.getName());
+//		System.out.println(s1.getAge());
+		OrderService os=container.getBean(OrderService.class);
+		os.placeOrder();
 	}
 
 }

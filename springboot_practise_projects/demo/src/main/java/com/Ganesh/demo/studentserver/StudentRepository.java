a68c3be7c4b3753a484b067ed6f8e35d0edd25e7
Ganesh <bandaruganesh876@gmail.com>
2026-07-13 20:29:52 +0530

    just to check the working

--- springboot_practise_projects/demo/src/main/java/com/Ganesh/demo/studentserver/StudentRepository.java
+++ springboot_practise_projects/demo/src/main/java/com/Ganesh/demo/studentserver/StudentRepository.java
@@ -1,8 +1,9 @@
 package com.Ganesh.demo.studentserver;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentRepository implements {
+public interface StudentRepository extends JpaRepository {
 
 }

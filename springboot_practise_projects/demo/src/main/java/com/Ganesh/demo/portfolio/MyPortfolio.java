73f8e0c3867a4ffb34282ee94280516f15cf0ae7
Ganesh <bandaruganesh876@gmail.com>
2026-07-14 11:00:49 +0530

    created -myself

---
+++ springboot_practise_projects/demo/src/main/java/com/Ganesh/demo/portfolio/MyPortfolio.java
@@ -0,0 +1,27 @@
+package com.Ganesh.demo.portfolio;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyPortfolio {
+    @GetMapping("/myself")
+    public String Myself(){
+        return """
+         <h1>I am Ganesh</h1>
+        <p>I am from Andhra Pradesh</p>
+        <ul>
+        <l1>Github Link: https://github.com/Ganesh9346</l1>
+        <l1>LeethCode Link: https://leetcode.com/u/8oF2mvGWYZ/</l1>
+        """;
+    }
+    @GetMapping("/skills")
+    public String skills(){
+        return """
+                <h3>I am Good at Building Backend Developemnt using spring and spring boot frame work</h3>
+                <h3>I know Programming languages such as C,Java,Java Script</h3>
+                <h3>Further more i also have a string knowledge in froentend developemnt using technologies as Java Script and React</h3>
+                
+                """
+    }
+}

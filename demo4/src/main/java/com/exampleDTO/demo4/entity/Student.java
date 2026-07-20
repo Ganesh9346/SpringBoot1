aadef0efb062f53eb021cad32db507382c44622e
Ganesh <bandaruganesh876@gmail.com>
2026-07-20 12:57:53 +0530

    scope

--- demo4/src/main/java/com/exampleDTO/demo4/entity/Student.java
+++ demo4/src/main/java/com/exampleDTO/demo4/entity/Student.java
@@ -4,10 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
 @Entity
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotBlank
     private Integer id;
     private String name;
     private String email;
---
+++ demo4/src/main/java/com/exampleDTO/demo4/scope/LpuStudent.java
@@ -0,0 +1,10 @@
+package com.exampleDTO.demo4.scope;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LpuStudent {
+    @Autowired
+    LPU lpu;
+}
